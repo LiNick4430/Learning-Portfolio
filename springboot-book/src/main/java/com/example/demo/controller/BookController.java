70ff49c939577376b292c6db102239c15e6dff2a
User <User@DESKTOP-G198TRC>
2025-09-24 07:01:22 +0800

    pc

--- springboot-book/src/main/java/com/example/demo/controller/BookController.java
+++ springboot-book/src/main/java/com/example/demo/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 import com.example.demo.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import com.example.demo.service.BookService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(value = {"http://localhost:5173"})
 public class BookController {
 
 	@Autowired
